e73ee61027b7a3e949a772fb3a52e944b727a03a
Aaron Batilo <AaronBatilo@gmail.com>
2023-11-18 09:50:57 -0700

    Run gpt3 and gpt4

---
+++ apis/gpt3turbo.js
@@ -0,0 +1,34 @@
+import http from "k6/http";
+
+export default function(
+  timePerToken,
+  tokensPerSecond,
+  completionTokensMetric,
+  secretKey) {
+  const url = 'https://api.openai.com/v1/chat/completions';
+  const body = JSON.stringify({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'user', content: 'What is the OpenAI mission?' },
+    ],
+  });
+  const params = {
+    headers: {
+      'Authorization': 'Bearer ' + secretKey,
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const resp = http.post(url, body, params);
+  const { usage: { completion_tokens } } = JSON.parse(resp.body);
+
+  const timeInMilliseconds = resp.timings.duration;
+
+  timePerToken.add(resp.timings.duration / completion_tokens);
+  tokensPerSecond.add(completion_tokens / (timeInMilliseconds / 1000));
+  completionTokensMetric.add(completion_tokens);
+
+  if (resp.status !== 200) {
+    console.error('Unexpected status code: ' + resp.status);
+  }
+}
--- main.js
+++ main.js
@@ -1,17 +1,40 @@
-import http from 'k6/http';
+import { Trend } from "k6/metrics";
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import gpt3turbo from './apis/gpt3turbo.js';
+import gpt4turbo from './apis/gpt4turbo.js';
 
 export const options = {
-  vus: 100,
+  vus: 10,
   duration: '10s',
 };
 
+const timePerToken = Trend('time_per_token (ms)');
+const tokensPerSecond = Trend('tokens_per_second');
+const completionTokensMetric = Trend('completion_tokens');
+
 export default function() {
-  http.get(`${__ENV.HOSTNAME}`);
+  const target = `${__ENV.TARGET}`;
+  if (target === 'gpt3turbo') {
+    const secretKey = `${__ENV.OPENAI_SECRET_KEY}`;
+    gpt3turbo(
+      timePerToken,
+      tokensPerSecond,
+      completionTokensMetric,
+      secretKey,
+    );
+  } else if (target === 'gpt4turbo') {
+    const secretKey = `${__ENV.OPENAI_SECRET_KEY}`;
+    gpt4turbo(
+      timePerToken,
+      tokensPerSecond,
+      completionTokensMetric,
+      secretKey,
+    );
+  }
 }
 
 export function handleSummary(data) {
-  const name = `${__ENV.HOSTNAME.replace(/\W/g, '_')}.html`;
+  const name = `${__ENV.TARGET.replace(/\W/g, '_')}.html`;
   return {
     [name]: htmlReport(data),
   };
